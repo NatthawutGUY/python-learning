bebf5d3e89b5b3b6bbb5e7f570dfbe028ba35076
Natthawut Putthong <68010375@kmitl.ac.th>
2026-07-16 23:32:54 +0700

    day04

---
+++ python30-day/day01/day04/str.py
@@ -0,0 +1,14 @@
+letter = 'P'
+print(letter)
+print(len(letter))
+letter = 'P'                # A string could be a single character or a bunch of texts
+print(letter)               # P
+print(len(letter))          # 1
+greeting = 'Hello, World!'  # String could be made using a single or double quote,"Hello, World!"
+print(greeting)             # Hello, World!
+print(len(greeting))        # 13
+sentence = "I hope you are enjoying 30 days of Python Challenge"
+print(sentence)
+
+multiline_string = ''' I am a teacher and enjoy teacher . I didn't find anything as rewarding as empoering people. that is why I created 30 days od python. '''
+print(multiline_string)
